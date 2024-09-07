25cccf186314de336cd35d2b25bd2a18fff86edf
hosnyegy2 <hosnyegy78@gmail.com>
2024-09-07 17:19:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -88,6 +88,6 @@ function openTabs(evt, tabsyName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(tabsyName).style.display = "flex";
+    document.getElementById(tabsyName).style.display = "block";
     evt.currentTarget.className += " active";
 }
