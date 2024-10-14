6083f513ce2c432cf34d071159908b635abd0ffe
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-14 21:38:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,3 @@
-jQuery(document).ready(function ($) {
-
-    "use strict";
- 
-    function openTabs(evt, tabsyName) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(tabsyName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
-    
-    function dodajAktywne(elem) {
-        var a = document.getElementsByTagName('button');
-        for (i = 0; i < a.length; i++) {
-            a[i].classList.remove('active')
-        }
-        elem.classList.add('active');
-    }
-
-    function setURL(url) {
-        document.getElementById('iframe').src = url;
-    }
-
-});
-//////////////////////////////////////
-function dodajAktywne(elem) {
-    // get all 'a' elements
-    var a = document.getElementsByTagName('button');
-    // loop through all 'a' elements
-    for (i = 0; i < a.length; i++) {
-        // Remove the class 'active' if it exists
-        a[i].classList.remove('active')
-    }
-    // add 'active' classs to the element that was clicked
-    elem.classList.add('active');
-}
-
-function setURL(url) {
-    document.getElementById('egy_strame').src = url;
-}
 ///////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 function openTabs(evt, tabsyName) {
@@ -61,7 +14,8 @@ function openTabs(evt, tabsyName) {
     evt.currentTarget.className += " active";
 }
 
-
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 function checkTime(showTime, hideTime, iframeSrc) {
   const now = new Date();
 
@@ -90,7 +44,8 @@ function startTimer(showTime, hideTime, iframeSrc) {
   // التحقق الفوري عند تحميل الصفحة
   checkTime(showTime, hideTime, iframeSrc);
 }
-
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 function checkTimeAndContent() {
         const oldContent = document.getElementById('old-content');
         const newContent = document.getElementById('new-content');
