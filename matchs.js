f968e62f4f1f7944b0d0d63bde4938fd26caca1b
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-15 16:07:18 +0300

    Add files via upload

---
+++ matchs.js
@@ -0,0 +1,100 @@
+// البيانات الديناميكية لكل يوم
+const matchData = {
+    "2024/10/15": [{
+            "id": 246,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "باراجواي",
+                "logo": "https://img.btolat.com/teamslogo/13867.png?v=404"
+            },
+            "fareq2": {
+                "name": "فنزويلا",
+                "logo": "https://img.btolat.com/teamslogo/17504.png?v=818"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 2:00 AM",
+            "timeEnd": "2024/10/16 4:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+            },
+        {
+            "id": 247,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "أوروجواي",
+                "logo": "https://img.btolat.com/teamslogo/17412.png?v=74"
+            },
+            "fareq2": {
+                "name": "الإكوادور",
+                "logo": "https://img.btolat.com/teamslogo/8848.png?v=92"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 2:00 AM",
+            "timeEnd": "2024/10/16 4:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+            },
+        {
+            "id": 248,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "الأرجنتين",
+                "logo": "https://img.btolat.com/teamslogo/5886.png?v=13"
+            },
+            "fareq2": {
+                "name": "بوليفيا",
+                "logo": "https://img.btolat.com/teamslogo/6979.png?v=155"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 3:00 AM",
+            "timeEnd": "2024/10/16 5:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+            },
+        {
+            "id": 249,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "البرازيل",
+                "logo": "https://img.btolat.com/teamslogo/7084.png?v=176"
+            },
+            "fareq2": {
+                "name": "بيرو",
+                "logo": "https://img.btolat.com/teamslogo/13898.png?v=739"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 3:45 AM",
+            "timeEnd": "2024/10/16 5:45 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+            }
+        ],
+    "2024/10/17": [{
+        "id": 244,
+        "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_15.html",
+        "fareq1": {
+            "name": "كوريا الجنوبية",
+            "logo": "https://img.btolat.com/teamslogo/12303.png?v=149"
+        },
+        "fareq2": {
+            "name": "العراق",
+            "logo": "https://img.btolat.com/teamslogo/11499.png?v=748"
+        },
+        "btola": "تصفيات آسيا لكأس العالم",
+        "timeStart": "2024/10/15 2:00 PM",
+        "timeEnd": "2024/10/15 4:00 PM",
+        "score": {
+            "team1": 0,
+            "team2": 0
+        }
+        }]
+};
+
