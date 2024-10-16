83a2389b6d2a5b8b88afe6d37887a75a1b990fd2
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-16 23:19:47 +0300

    Create moled.js

---
+++ moled.js
@@ -0,0 +1,218 @@
+// البيانات الديناميكية لكل يوم
+const matchData = {
+    "2024/10/15": [{
+            "id": 244,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_79.html",
+            "fareq1": {
+                "name": "كولومبيا",
+                "logo": "https://img.btolat.com/teamslogo/8117.png?v=867"
+            },
+            "fareq2": {
+                "name": "تشيلي",
+                "logo": "https://img.btolat.com/teamslogo/7959.png?v=402"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/15 11:30 PM",
+            "timeEnd": "2024/10/16 1:30 AM",
+            "score": {
+                "team1": 4,
+                "team2": 0
+            }
+        },
+    ],
+    "2024/10/16": [
+    
+        {
+            "id": 246,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_6.html",
+            "fareq1": {
+                "name": "باراجواي",
+                "logo": "https://img.btolat.com/teamslogo/13867.png?v=404"
+            },
+            "fareq2": {
+                "name": "فنزويلا",
+                "logo": "https://img.btolat.com/teamslogo/17504.png?v=818"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 2:00 AM",
+            "timeEnd": "2024/10/16 4:00 AM",
+            "score": {
+                "team1": 2,
+                "team2": 1
+            }
+        },
+        {
+            "id": 247,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_19.html",
+            "fareq1": {
+                "name": "أوروجواي",
+                "logo": "https://img.btolat.com/teamslogo/17412.png?v=74"
+            },
+            "fareq2": {
+                "name": "الإكوادور",
+                "logo": "https://img.btolat.com/teamslogo/8848.png?v=92"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 2:30 AM",
+            "timeEnd": "2024/10/16 4:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+        },
+        {
+            "id": 248,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_98.html",
+            "fareq1": {
+                "name": "الأرجنتين",
+                "logo": "https://img.btolat.com/teamslogo/5886.png?v=13"
+            },
+            "fareq2": {
+                "name": "بوليفيا",
+                "logo": "https://img.btolat.com/teamslogo/6979.png?v=155"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 3:00 AM",
+            "timeEnd": "2024/10/16 5:00 AM",
+            "score": {
+                "team1": 6,
+                "team2": 0
+            }
+        },
+        {
+            "id": 249,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_73.html",
+            "fareq1": {
+                "name": "البرازيل",
+                "logo": "https://img.btolat.com/teamslogo/7084.png?v=176"
+            },
+            "fareq2": {
+                "name": "بيرو",
+                "logo": "https://img.btolat.com/teamslogo/13898.png?v=739"
+            },
+            "btola": " تصفيات أمريكا الجنوبية لكأس العالم ",
+            "timeStart": "2024/10/16 3:45 AM",
+            "timeEnd": "2024/10/16 5:45 AM",
+            "score": {
+                "team1": 4,
+                "team2": 0
+            }
+        },
+        {
+            "id": 258,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_16.html",
+            "fareq1": {
+                "name": "يوفنتوس",
+                "logo": "https://img.btolat.com/teamslogo/11922.png?v=381"
+            },
+            "fareq2": {
+                "name": "بايرن ميونخ",
+                "logo": "https://img.btolat.com/teamslogo/10285.png?v=46"
+            },
+            "btola": "دوري أبطال أوروبا للسيدات",
+            "timeStart": "2024/10/16 7:45 PM",
+            "timeEnd": "2024/10/16 9:45 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+        },
+        {
+            "id": 259,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_56.html",
+            "fareq1": {
+                "name": "بولتن",
+                "logo": "https://logowik.com/content/uploads/images/t_club-union-deportiva-norte-de-dean-funes-cordoba6095.logowik.com.webp"
+            },
+            "fareq2": {
+                "name": "مانشستر سيتي",
+                "logo": "https://img.btolat.com/teamslogo/9259.png?v=545"
+            },
+            "btola": "دوري أبطال أوروبا للسيدات",
+            "timeStart": "2024/10/16 7:45 PM",
+            "timeEnd": "2024/10/16 9:45 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+        },
+        {
+            "id": 260,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_33.html",
+            "fareq1": {
+                "name": "آرسنال",
+                "logo": "https://img.btolat.com/teamslogo/9002.png?v=593"
+            },
+            "fareq2": {
+                "name": "فاليرينغا",
+                "logo": "https://logowik.com/content/uploads/images/t_valerenga-oslo2898.logowik.com.webp"
+            },
+            "btola": "دوري أبطال أوروبا للسيدات",
+            "timeStart": "2024/10/16 10:00 PM",
+            "timeEnd": "2024/10/17 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+        },
+        {
+            "id": 261,
+            "gameUrl": "https://sport.coldady.com/2024/10/2024-2025_30.html",
+            "fareq1": {
+                "name": "برشلونة",
+                "logo": "https://img.btolat.com/teamslogo/15702.png?v=616"
+            },
+            "fareq2": {
+                "name": "هاماربي",
+                "logo": "https://logowik.com/content/uploads/images/t_738_marmaray.jpg"
+            },
+            "btola": "دوري أبطال أوروبا للسيدات",
+            "timeStart": "2024/10/16 10:00 PM",
+            "timeEnd": "2024/10/17 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+        }
+    ],
+    "2024/10/17": [
+        {
+            "id": 256,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "غلطة سراي",
+                "logo": "https://img.btolat.com/teamslogo/17029.png?v=354"
+            },
+            "fareq2": {
+                "name": "روما",
+                "logo": "https://img.btolat.com/teamslogo/11998.png?v=815"
+            },
+            "btola": "دوري أبطال أوروبا للسيدات",
+            "timeStart": "2024/10/17 7:45 PM",
+            "timeEnd": "2024/10/17 9:45 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+        },
+        {
+            "id": 257,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "ريال مدريد",
+                "logo": "https://img.btolat.com/teamslogo/16110.png?v=376"
+            },
+            "fareq2": {
+                "name": "سيلتك",
+                "logo": "https://upload.wikimedia.org/wikipedia/ar/thumb/3/35/Celtic_FC.svg/180px-Celtic_FC.svg.png"
+            },
+            "btola": "دوري أبطال أوروبا للسيدات",
+            "timeStart": "2024/10/17 7:45 PM",
+            "timeEnd": "2024/10/17 9:45 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+        }
+    ]
+};
