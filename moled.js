173f3a405894e63e4fa846b074dd761e7dbe23eb
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-19 18:39:43 +0300

    Update moled.js

--- moled.js
+++ moled.js
@@ -234,7 +234,160 @@ const matchData = {
             }
 }
     ],
-    "2024/10/20": [],
+    "2024/10/20": [
+        {
+            "id": 286,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "إمبولي",
+                "logo": "https://img.btolat.com/teamslogo/11867.png?v=633"
+            },
+            "fareq2": {
+                "name": "نابولي",
+                "logo": "https://img.btolat.com/teamslogo/11947.png?v=385"
+            },
+            "btola": "الدوري الايطالي",
+            "timeStart": "2024/10/20 1:30 PM",
+            "timeEnd": "2024/10/20 3:30 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 287,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "ولفرهامبتون",
+                "logo": "https://img.btolat.com/teamslogo/9446.png?v=997"
+            },
+            "fareq2": {
+                "name": "مانشستر سيتي",
+                "logo": "https://img.btolat.com/teamslogo/9259.png?v=545"
+            },
+            "btola": "الدوري الإنجليزي",
+            "timeStart": "2024/10/20 4:00 PM",
+            "timeEnd": "2024/10/20 6:00 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 288,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "بيراميدز",
+                "logo": "https://img.btolat.com/teamslogo/23165.png?v=927"
+            },
+            "fareq2": {
+                "name": "الزمالك",
+                "logo": "https://img.btolat.com/teamslogo/8959.png?v=450"
+            },
+            "btola": "كاس السوبر المصري",
+            "timeStart": "2024/10/20 4:05 PM",
+            "timeEnd": "2024/10/20 6:05 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 289,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "أتلتيكو مدريد",
+                "logo": "https://img.btolat.com/teamslogo/15692.png?v=534"
+            },
+            "fareq2": {
+                "name": "ليجانيس",
+                "logo": "https://img.btolat.com/teamslogo/15833.png?v=457"
+            },
+            "btola": "الدوري الاسباني",
+            "timeStart": "2024/10/20 5:15 PM",
+            "timeEnd": "2024/10/20 7:15 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 290,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "ليفربول",
+                "logo": "https://img.btolat.com/teamslogo/9249.png?v=24"
+            },
+            "fareq2": {
+                "name": "تشيلسي",
+                "logo": "https://img.btolat.com/teamslogo/9092.png?v=38"
+            },
+            "btola": "الدوري الإنجليزي",
+            "timeStart": "2024/10/20 6:30 PM",
+            "timeEnd": "2024/10/20 8:30 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 291,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "الأهلي",
+                "logo": "https://img.btolat.com/teamslogo/8883.png?v=658"
+            },
+            "fareq2": {
+                "name": "سيراميكا كليوباترا",
+                "logo": "https://img.btolat.com/teamslogo/33716.png?v=636"
+            },
+            "btola": "كاس السوبر المصري",
+            "timeStart": "2024/10/20 8:00 PM",
+            "timeEnd": "2024/10/20 10:00 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 292,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "روما",
+                "logo": "https://img.btolat.com/teamslogo/11998.png?v=815"
+            },
+            "fareq2": {
+                "name": "إنتر ميلان",
+                "logo": "https://img.btolat.com/teamslogo/11917.png?v=328"
+            },
+            "btola": "الدوري الايطالي",
+            "timeStart": "2024/10/20 9:45 PM",
+            "timeEnd": "2024/10/20 11:45 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 293,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "برشلونة",
+                "logo": "https://img.btolat.com/teamslogo/15702.png?v=616"
+            },
+            "fareq2": {
+                "name": "إشبيلية",
+                "logo": "https://img.btolat.com/teamslogo/16175.png?v=306"
+            },
+            "btola": "الدوري الاسباني",
+            "timeStart": "2024/10/20 10:00 PM",
+            "timeEnd": "2024/10/21 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+}
+    ],
     "2024/10/21": [],
     "2024/10/22": [],
     "2024/10/23": [],
