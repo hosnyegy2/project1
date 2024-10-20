8fd68a31d4f4ee9f5818ef1868dc3314618f9e53
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-20 21:32:21 +0300

    Update moled.js

--- moled.js
+++ moled.js
@@ -159,7 +159,103 @@ const matchData = {
             }
 }
     ],
-    "2024/10/21": [],
+    "2024/10/21": [
+        {
+            "id": 295,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "سبورتنج",
+                "logo": "https://img.btolat.com/teamslogo/35557.png?v=308"
+            },
+            "fareq2": {
+                "name": "المقاولون العرب",
+                "logo": "https://img.btolat.com/teamslogo/16175.png?v=306"
+            },
+            "btola": "دوري الدرجة الثانية المصري",
+            "timeStart": "2024/10/21 3:30 PM",
+            "timeEnd": "2024/10/21 5:30 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 296,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "السد",
+                "logo": "https://img.btolat.com/teamslogo/14503.png?v=105"
+            },
+            "fareq2": {
+                "name": "برسبوليس",
+                "logo": "https://img.btolat.com/teamslogo/11477.png?v=939"
+            },
+            "btola": "دوري أبطال آسيا",
+            "timeStart": "2024/10/21 7:00 PM",
+            "timeEnd": "2024/10/21 9:00 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 297,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "العين",
+                "logo": "https://img.btolat.com/teamslogo/17231.png?v=104"
+            },
+            "fareq2": {
+                "name": "الهلال السعودي",
+                "logo": "https://img.btolat.com/teamslogo/15001.png?v=23"
+            },
+            "btola": "دوري أبطال آسيا",
+            "timeStart": "2024/10/21 7:00 PM",
+            "timeEnd": "2024/10/21 9:00 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 298,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "الريان",
+                "logo": "https://img.btolat.com/teamslogo/14502.png?v=750"
+            },
+            "fareq2": {
+                "name": "الأهلي السعودي",
+                "logo": "https://img.btolat.com/teamslogo/14990.png?v=643"
+            },
+            "btola": "دوري أبطال آسيا",
+            "timeStart": "2024/10/21 9:00 PM",
+            "timeEnd": "2024/10/21 11:00 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 299,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "فالنسيا",
+                "logo": "https://img.btolat.com/teamslogo/16261.png?v=12"
+            },
+            "fareq2": {
+                "name": "لاس بالماس",
+                "logo": "https://img.btolat.com/teamslogo/16040.png?v=452"
+            },
+            "btola": "الدوري الاسباني",
+            "timeStart": "2024/10/21 10:00 PM",
+            "timeEnd": "2024/10/22 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+}
+    ],
     "2024/10/22": [],
     "2024/10/23": [],
     "2024/10/24": [],
