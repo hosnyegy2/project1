85a6a4c4832d361bec3fdc3d2e5b89a13b5aaaf1
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-21 21:51:45 +0300

    Update moled.js

--- moled.js
+++ moled.js
@@ -103,7 +103,122 @@ const matchData = {
             }
 }
     ],
-    "2024/10/22": [],
+    "2024/10/22": [
+        {
+            "id": 300,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "استقلال طهران",
+                "logo": "https://img.btolat.com/teamslogo/11450.png"
+            },
+            "fareq2": {
+                "name": "النصر السعودي",
+                "logo": "https://img.btolat.com/teamslogo/15010.png?v=992"
+            },
+            "btola": "دوري أبطال آسيا",
+            "timeStart": "2024/10/22 7:00 PM",
+            "timeEnd": "2024/10/22 9:00 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 301,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "ميلان",
+                "logo": "https://img.btolat.com/teamslogo/11938.png?v=918"
+            },
+            "fareq2": {
+                "name": "كلوب بروج",
+                "logo": "https://img.btolat.com/teamslogo/6752.png?v=734"
+            },
+            "btola": "دوري أبطال أوروبا",
+            "timeStart": "2024/10/22 7:45 PM",
+            "timeEnd": "2024/10/22 9:45 PM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 302,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "يوفنتوس",
+                "logo": "https://img.btolat.com/teamslogo/11922.png?v=381"
+            },
+            "fareq2": {
+                "name": "شتوتجارت",
+                "logo": "https://img.btolat.com/teamslogo/10646.png?v=579"
+            },
+            "btola": "دوري أبطال أوروبا",
+            "timeStart": "2024/10/22 10:00 PM",
+            "timeEnd": "2024/10/23 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 303,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "باريس سان جيرمان",
+                "logo": "https://img.btolat.com/teamslogo/10061.png?v=97"
+            },
+            "fareq2": {
+                "name": "ايندهوفن",
+                "logo": "https://img.btolat.com/teamslogo/13364.png?v=742"
+            },
+            "btola": "دوري أبطال أوروبا",
+            "timeStart": "2024/10/22 10:00 PM",
+            "timeEnd": "2024/10/23 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 304,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "آرسنال",
+                "logo": "https://img.btolat.com/teamslogo/9002.png?v=593"
+            },
+            "fareq2": {
+                "name": "شاختار دونيتسك",
+                "logo": "https://img.btolat.com/teamslogo/17205.png?v=658"
+            },
+            "btola": "دوري أبطال أوروبا",
+            "timeStart": "2024/10/22 10:00 PM",
+            "timeEnd": "2024/10/23 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+},
+        {
+            "id": 305,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "ريال مدريد",
+                "logo": "https://img.btolat.com/teamslogo/16110.png?v=376"
+            },
+            "fareq2": {
+                "name": "بوروسيا دورتموند",
+                "logo": "https://img.btolat.com/teamslogo/10303.png?v=260"
+            },
+            "btola": "دوري أبطال أوروبا",
+            "timeStart": "2024/10/22 10:00 PM",
+            "timeEnd": "2024/10/23 12:00 AM",
+            "score": {
+                "team1": 0,
+                "team2": 0
+            }
+}
+    ],
     "2024/10/23": [],
     "2024/10/24": [],
     "2024/10/25": [],
