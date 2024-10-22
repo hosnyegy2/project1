48a516ade91de920544c55ea97d76ad228b6ecbf
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-22 22:50:29 +0300

    Update moled.js

--- moled.js
+++ moled.js
@@ -6,7 +6,103 @@ const matchData = {
     "2024/10/18": [],
     "2024/10/19": [],
     "2024/10/20": [],
-    "2024/10/21": [],
+    "2024/10/21": [
+        {
+            "id": 295,
+            "gameUrl": "",
+            "fareq1": {
+                "name": "سبورتنج",
+                "logo": "https://img.btolat.com/teamslogo/35557.png?v=308"
+            },
+            "fareq2": {
+                "name": "المقاولون العرب",
+                "logo": "https://img.btolat.com/teamslogo/8884.png?v=28"
+            },
+            "btola": "دوري الدرجة الثانية المصري",
+            "timeStart": "2024/10/21 3:30 PM",
+            "timeEnd": "2024/10/21 5:30 PM",
+            "score": {
+                "team1": 1,
+                "team2": 1
+            }
+            },
+        {
+            "id": 296,
+            "gameUrl": "https://sport.coldady.com/2024/10/blog-post_21.html",
+            "fareq1": {
+                "name": "السد",
+                "logo": "https://img.btolat.com/teamslogo/14503.png?v=105"
+            },
+            "fareq2": {
+                "name": "برسبوليس",
+                "logo": "https://img.btolat.com/teamslogo/11477.png?v=939"
+            },
+            "btola": "دوري أبطال آسيا",
+            "timeStart": "2024/10/21 7:00 PM",
+            "timeEnd": "2024/10/21 9:00 PM",
+            "score": {
+                "team1": 1,
+                "team2": 0
+            }
+            },
+        {
+            "id": 297,
+            "gameUrl": "https://sport.coldady.com/2024/10/blog-post_77.html",
+            "fareq1": {
+                "name": "العين",
+                "logo": "https://img.btolat.com/teamslogo/17231.png?v=104"
+            },
+            "fareq2": {
+                "name": "الهلال السعودي",
+                "logo": "https://img.btolat.com/teamslogo/15001.png?v=23"
+            },
+            "btola": "دوري أبطال آسيا",
+            "timeStart": "2024/10/21 7:00 PM",
+            "timeEnd": "2024/10/21 9:00 PM",
+            "score": {
+                "team1": 4,
+                "team2": 5
+            }
+            },
+        {
+            "id": 298,
+            "gameUrl": "https://sport.coldady.com/2024/10/blog-post_52.html",
+            "fareq1": {
+                "name": "الريان",
+                "logo": "https://img.btolat.com/teamslogo/14502.png?v=750"
+            },
+            "fareq2": {
+                "name": "الأهلي السعودي",
+                "logo": "https://img.btolat.com/teamslogo/14990.png?v=643"
+            },
+            "btola": "دوري أبطال آسيا",
+            "timeStart": "2024/10/21 9:00 PM",
+            "timeEnd": "2024/10/21 11:00 PM",
+            "score": {
+                "team1": 0,
+                "team2": 2
+            }
+            },
+        {
+            "id": 299,
+            "gameUrl": "https://sport.coldady.com/2024/10/valencia-vs-las-palmas.html",
+            "fareq1": {
+                "name": "فالنسيا",
+                "logo": "https://img.btolat.com/teamslogo/16261.png?v=12"
+            },
+            "fareq2": {
+                "name": "لاس بالماس",
+                "logo": "https://img.btolat.com/teamslogo/16040.png?v=452"
+            },
+            "btola": "الدوري الاسباني",
+            "timeStart": "2024/10/21 10:00 PM",
+            "timeEnd": "2024/10/22 12:00 AM",
+            "score": {
+                "team1": 1,
+                "team2": 0
+            }
+            }
+        ],
     "2024/10/22": [
         {
             "id": 300,
@@ -42,7 +138,7 @@ const matchData = {
             "timeStart": "2024/10/22 7:45 PM",
             "timeEnd": "2024/10/22 9:45 PM",
             "score": {
-                "team1": 2,
+                "team1": 3,
                 "team2": 1
             }
 },
@@ -81,7 +177,7 @@ const matchData = {
             "timeEnd": "2024/10/23 12:00 AM",
             "score": {
                 "team1": 0,
-                "team2": 0
+                "team2": 1
             }
 },
         {
@@ -99,7 +195,7 @@ const matchData = {
             "timeStart": "2024/10/22 10:00 PM",
             "timeEnd": "2024/10/23 12:00 AM",
             "score": {
-                "team1": 0,
+                "team1": 1,
                 "team2": 0
             }
 },
@@ -119,7 +215,7 @@ const matchData = {
             "timeEnd": "2024/10/23 12:00 AM",
             "score": {
                 "team1": 0,
-                "team2": 0
+                "team2": 2
             }
 }
     ],
