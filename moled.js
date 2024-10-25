09f9218a37a490dcc41951b67685910c28f0c06f
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-25 18:21:43 +0300

    Update moled.js

--- moled.js
+++ moled.js
@@ -563,7 +563,7 @@ const matchData = {
             "timeEnd": "2024/10/25 8:05 PM",
             "score": {
                 "team1": 1,
-                "team2": 0
+                "team2": 1
             }
 },
         {
