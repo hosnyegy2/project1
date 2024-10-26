2cc412d5d68398d96c2b101046502c2fabe4511a
hosnyegy2 <hosnyegy78@gmail.com>
2024-10-26 19:40:18 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -18,7 +18,6 @@ jQuery(document).ready(function ($) {
             r = moment(a, "YYYY/MM/DD h:mm A"),
             n = moment(e, "YYYY/MM/DD h:mm A"),
             s = moment.utc().format("YYYY/MM/DD h:mm A");
-             hasExtraTime = t.data("extra-time"); // Data attribute to determine if the match has extra time
 
         // التحقق مما إذا كان التوقيت الصيفي مفعلًا
         var hoursToSubtract = isDaylightSavingTime() ? 3 : 2; // 3 ساعات للتوقيت الصيفي و 2 للشتوي
@@ -63,58 +62,43 @@ jQuery(document).ready(function ($) {
                 t.parent().parent().parent().parent().find(".hoverG div").html("شاهد المبارة الان");
                 
                 
-                  // Add timer functionality
-    var timerElement = t.parent().parent().parent().parent().find(".timer");
-    var statusElement = t.parent().parent().parent().parent().find(".status");
-    var startTime = moment.utc(a).subtract(hoursToSubtract, "hours").toDate();
-    var delayEndTime = moment(startTime).add(3, "minutes").toDate();
-    var firstHalfEndTime = moment(delayEndTime).add(45, "minutes").toDate();
-    var halfTimeEndTime = moment(firstHalfEndTime).add(15, "minutes").toDate();
-    var secondHalfEndTime = moment(halfTimeEndTime).add(45, "minutes").toDate();
+                // Add timer functionality
+                var timerElement = t.parent().parent().parent().parent().find(".timer");
+                var statusElement = t.parent().parent().parent().parent().find(".status"); // Add a new element to display the status
+                var startTime = moment.utc(a).subtract(hoursToSubtract, "hours").toDate();
+                var delayEndTime = moment(startTime).add(3, "minutes").toDate(); // Add a 5-minute delay
+                var firstHalfEndTime = moment(delayEndTime).add(45, "minutes").toDate();
+                var halfTimeEndTime = moment(firstHalfEndTime).add(15, "minutes").toDate();
+                var secondHalfEndTime = moment(halfTimeEndTime).add(45, "minutes").toDate();
 
-    var extraTimeEnd = null;
-    if (hasExtraTime) {
-        // Only calculate extra time if the match has extra time
-        extraTimeEnd = moment(secondHalfEndTime).add(30, "minutes").toDate();
-    }
+                var timerInterval = setInterval(function () {
+                    var currentTime = moment.utc().toDate();
+                    if (currentTime < delayEndTime) {
+                        timerElement.html("<span class='waiting-kick-off'>بانتظار ضربة البداية</span>"); // Display message during the 5-minute delay
+                        statusElement.text(""); // Clear the status element
+                    } else if (currentTime < firstHalfEndTime) {
+                        var timeElapsed = moment(currentTime).diff(delayEndTime);
+                        var minutes = Math.floor(timeElapsed / 60000);
+                        var seconds = Math.floor((timeElapsed % 60000) / 1000);
+                        timerElement.html("`" + minutes + ":" + seconds.toString().padStart(2, "0")); // Add dot above the timer
+                        statusElement.text("الشوط الاول"); // Display "First Half" during the first 45 minutes
+                    } else if (currentTime < halfTimeEndTime) {
+                        timerElement.html("`45:00"); // Display "45:00" during the half-time break with apostrophe
+                        statusElement.text("استراحة"); // Display "Break" during the half-time break
+                    } else if (currentTime < secondHalfEndTime) {
+                        var timeElapsed = moment(currentTime).diff(halfTimeEndTime);
+                        var minutes = Math.floor(timeElapsed / 60000) + 45; // Add 45 to continue from the second half
+                        var seconds = Math.floor((timeElapsed % 60000) / 1000);
+                        timerElement.html("`" + minutes + ":" + seconds.toString().padStart(2, "0")); // Add dot above the timer
+                        statusElement.text("الشوط الثانى"); // Display "Second Half" during the second 45 minutes
+                    } else {
+                        clearInterval(timerInterval);
+                        timerElement.html("`90:00"); // Display "90:00" at the end of the match with apostrophe
+                        statusElement.html("<span class='full-time'>Full Time</span>"); // Display "Full Time" at the end of the match
+                        //t.parent().parent().parent().parent().find(".timer-status").remove(); // Hide the entire timer-status div
+                    }
+                }, 1000);
 
-    var timerInterval = setInterval(function () {
-        var currentTime = moment.utc().toDate();
-
-        if (currentTime < delayEndTime) {
-            timerElement.html("<span class='waiting-kick-off'>بانتظار ضربة البداية</span>");
-            statusElement.text(""); // Clear the status element
-        } else if (currentTime < firstHalfEndTime) {
-            var timeElapsed = moment(currentTime).diff(delayEndTime);
-            var minutes = Math.floor(timeElapsed / 60000);
-            var seconds = Math.floor((timeElapsed % 60000) / 1000);
-            timerElement.html("`" + minutes + ":" + seconds.toString().padStart(2, "0"));
-            statusElement.text("الشوط الاول");
-        } else if (currentTime < halfTimeEndTime) {
-            timerElement.html("`45:00");
-            statusElement.text("استراحة");
-        } else if (currentTime < secondHalfEndTime) {
-            var timeElapsed = moment(currentTime).diff(halfTimeEndTime);
-            var minutes = Math.floor(timeElapsed / 60000) + 45;
-            var seconds = Math.floor((timeElapsed % 60000) / 1000);
-            timerElement.html("`" + minutes + ":" + seconds.toString().padStart(2, "0"));
-            statusElement.text("الشوط الثانى");
-        } else if (hasExtraTime && currentTime < extraTimeEnd) {
-            // Extra Time logic
-            var timeElapsed = moment(currentTime).diff(secondHalfEndTime);
-            var minutes = Math.floor(timeElapsed / 60000) + 90;
-            var seconds = Math.floor((timeElapsed % 60000) / 1000);
-            timerElement.html("`" + minutes + ":" + seconds.toString().padStart(2, "0"));
-            statusElement.text("وقت إضافى");
-        } else {
-            clearInterval(timerInterval);
-            timerElement.html("`90:00");
-            statusElement.html("<span class='full-time'>Full Time</span>");
-        }
-    }, 1000);
-
-
-                
                 
               // Add a new element to display the progress bar
             var progressBarElement = t.parent().parent().parent().parent().find(".progress-bar-container");
@@ -173,8 +157,6 @@ jQuery(document).ready(function ($) {
                     //t.parent().parent().parent().parent().find(".timer-status").remove(); // Hide the entire timer-status div
                 }
             }, 1000);
-
-
       
                 break;
 
