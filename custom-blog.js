149ca45023e99ba4c74ab4c6cf021aa4f339a295
hosnyegy2 <hosnyegy78@gmail.com>
2024-12-22 21:41:49 +0200

    Update custom-blog.js

--- custom-blog.js
+++ custom-blog.js
@@ -122,8 +122,6 @@ jQuery(document).ready(function ($) {
 
             default:
                 t.parent().find(".result_match").addClass("result_show");
-                t.parent().parent().parent().parent().find(".Fareeq-c span.bouton").html("انتهت"),
-                t.parent().parent().parent().parent().find(".hoverG div").html("انتهت المباراة"),
                 t.parent().parent().parent().parent().find(".timer-status").remove();
         }
     });
